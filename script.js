63d7afd245f7da69232af171eae87b985ceb9c48
Ikechukwu Chukwu <imchukwu@gmail.com>
2023-08-28 16:03:15 +0100

    pushing

--- script.js
+++ script.js
@@ -1,54 +1,49 @@
-"use strict"
-const secretNumder = Math.trunc(Math.random() * 20) + 1;
-
-let score = 20
-
-
+'use strict'
 // console.log(document.querySelector('.message').textContent);
+// (document.querySelector('.message').textContent) = 'Correct Number'
 
-// document.querySelector('.message').textContent = 'Correct number';
+// document.querySelector('.number').textContent = 13
+// document.querySelector('.score').textContent = 21
+// document.querySelector('.guess').value = 30;
 
-// .textContent = 13;
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector('.check').addEventListener('click', function(){
 
-// document.querySelector('.score').textContent= 20;
+    console.log(document.querySelector('.guess').value);
+    
+let guess = Number(document.querySelector('.guess').value);
+console.log(guess);
 
-// document.querySelector('.guess').value= 30;
+if (!guess)
+  document.querySelector('.message').textContent = 'Enter a number'
 
-document.querySelector('.check').addEventListener('click', function () {
-    console.log(document.querySelector('.guess').value)
+else if (guess === secretNumber){
+  document.querySelector('.message').textContent = 'Correct number';
+  document.querySelector('.number').textContent = secretNumber;
+  document.querySelector('body').style.backgroundColor = '#60b347';
+  document.querySelector('.number').style.width = '30rem';
+}
 
-    let guess = Number(document.querySelector('.guess').value)
-    console.log(guess)
-   if (!guess) {
-        document.querySelector('.message').textContent = 'Enter a number'
-    }
-    else if (guess === secretNumder) {
-        document.querySelector('.message').textContent = 'Correct number'
-        document.querySelector('body').style.backgroundColor = #606357
-        document.querySelector('.number').style.width = '30ren'
-    }
+else if (guess > secretNumber) {
+  if(score>0){
+  document.querySelector('.message').textContent = 'too high';
+  --score;
+  document.querySelector('.score').textContent = score;
+      }
+    else{
+    document.querySelector('.message').textContent = 'game over';
+}      
+}
 
-    else if (guess > secretNumder) {
-        if (score>0){
-            document.querySelector('.message').textContent = 'Too High'
-            --score;
-            document.querySelector('.score').textContent= score;
-        }
-        else{
-            document.querySelector('.message').textContent = 'Game Over'
-        }
-        
-    }
-    else if (guess < secretNumder) {
-        if(score>0){
-            document.querySelector('.message').textContent = 'Too Low'
-            --score;
-            document.querySelector('.score').textContent= score;
-        }
-        else{
-            document.querySelector('.message').textContent = 'Game Over'
-        }
-     
+else if (guess < secretNumber) {
+  if(score<0);
+  document.querySelector('.message').textContent = 'too low';
+  --score;
+  document.querySelector('.score').textContent = score;
+      }
+
+else{
+        document.querySelector('.message').textContent = 'game over';
+    }  
 })
-
-
